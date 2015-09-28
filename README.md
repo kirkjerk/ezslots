f2c36211a66e4acd2257e0b810de92bbfc36b687
kirkjerk <kirkjerk@gmail.com>
2015-09-27 20:17:55 -0400

    initial load

---
+++ README.md
@@ -0,0 +1,59 @@
+ezSlots
+=======
+ezSlots is a trivially simple JQuery "Slot Machine Construction Kit". Its only dependencies are jQuery and the jQuery Easing library. It can be allowed to run randomly or to go to a pre-specified "winning" combo, can have any number of wheels, and use any set of symbols (HTML characters, images, etc)
+
+It is designed as an educational tool / sample as well as a "ready to go" piece of technology to add some slots to any page. There are other, fancier jQuery slot machines out there (including the ability to keep spinning indefinitely, which ezSlots does not have) but by keeping the code base to under 100 lines, ezSlots is meant to be easy to understand and customize.
+
+Minimal Usage
+-----
+    <link href="ezslots.css" rel="stylesheet" type="text/css" />
+    <script src="jquery-1.11.3.js"></script>
+    <script src="jquery.easing.1.3.js"></script>
+    <script src="ezslots.js"></script>
+    <script>
+    $(document).ready(function(){
+       var slotMachine = new EZSlots("elementid",{});
+       var results = slotMachine.spin(); //.win() would force "winning" spin    
+     });
+    </script>
+    <div id="elementid"></div>
+
+Options
+-------
+The first argument is the id of a div existing on the page.
+
+The second argument is a map with the following keys:
+
+ - reelCount - how many reels/windows to display (default:3)
+ - symbols - an array containing html strings (or an array of arrays if you want different symbol sets for each reel) (default:['A','B','C']
+ - winningSet - an optional array specifying the offset of a "winning" spin. (For example, if the first entry in the symbols array was the "winner", this should be [0,0,0]) (default: none. If you want to call .win() this should be explicitly set)
+ - width - width of each slot window in pixels (default: 100)
+ - height - height of each slot window in pixels (default: 100)
+
+Samples
+-------
+There is a [sample page](http://kirkdev.alienbill.com/ezslots/sample.html) where you can see the "all defaults version", one with simple text numbers, one with images, and one that uses mixed type.
+
+Understanding the Code
+----------------------
+The code starts with the pattern as storing "this" in a private variable "that". It then sets member variables with a mix of values from the options map and hard-wired defaults.
+
+The init function is defined but only called once the other member functions are set. It creates the appropriate number of window/reels.
+
+The CSS suggest the structure ezslots generates:
+
+    .ezslots>.window>.slider>.symbol>.content
+
+* ezslots - a class assigned to the containing div
+* window - refers one of the reel-viewing windows
+* slider - this is the part that actually moves.
+* symbol - container for a single symbol that will show up in the window
+* content - where the symbol actually "lives" (needed for horizontal and vertical centering)
+
+Heights and width are manually and redundantly applied to ensure consistency with the animation.
+
+Also, each reel window is given the class "window_#" where # is its position in the ezslots div.
+
+For the animation effect, 20 symbols are added to each window (randomly selected from the array of options for that reel, except possibly for the "winning" result that actually shows up in the window when things come to rest) and an "easeOutElastic" easing gives the illusion of a rotating reel that then settles into place.
+
+With a regular or guaranteed win, the results are return an array of offsets into the array of "possibilities.") There's currently no function provided for translating these offsets into the html strings being displayed, but it would be trivial to write if needed.
---
+++ ezslots.js
@@ -0,0 +1,87 @@
+function EZSlots(id,useroptions){
+	var that = this; //keep reference to function for use in callbacks
+	//set some variables from the options, or with defaults.
+	var options = useroptions ? useroptions : {};
+	this.reelCount = options.reelCount ? options.reelCount : 3; //how many reels, assume 3 
+	this.symbols = options.symbols ? options.symbols : ['A','B','C'];
+	this.sameSymbolsEachSlot = true;
+	this.winningSet = options.winningSet;
+	this.width = options.width ? options.width : 100;
+	this.height = options.width ? options.height : 100;
+	this.howManySymbolsToAppend = 20; //how many symbols each spin adds
+	this.endingLocation = 7; //location for selected symbol... needs to be a few smaller than howManySymbolsToAppend
+	this.time = 6500; //time in millis for a spin to take
+	this.jqo = $("#"+id); //jquery object reference to main wrapper
+	this.jqoSliders = []; //jquery object reference to strips sliding up and down
+
+	//to initialize we construct the correct number of slot windows
+	//and then populate each strip once
+	this.init = function(){
+		this.jqo.addClass("ezslots"); //to get the css goodness
+		//figure out if we are using the same of symbols for each window - assume if the first 
+		//entry of the symbols is not a string we have an array of arrays
+		if(typeof this.symbols[0] != 'string'){
+			this.sameSymbolsEachSlot = false;
+		}
+		//make each slot window
+		for(var i = 0; i < this.reelCount; i++){
+			var jqoSlider = $('<div class="slider"></div>');
+			var jqoWindow = $('<div class="window window_"'+i+'></div>');
+			this.scaleJqo(jqoWindow).append(jqoSlider); //make window right size and put slider in it
+			this.jqo.append(jqoWindow); //add window to main div
+			this.jqoSliders.push(jqoSlider); //keep reference to jqo of slider
+			this.addSymbolsToStrip(jqoSlider,i); //and add the initial set 
+		}
+	};
+	//convenience function since we need to apply width and height to multiple parts
+	this.scaleJqo = function(jqo){
+		jqo.css("height",this.height+"px").css("width",this.width+"px");
+		return jqo;
+	}
+	//add the various symbols - but take care to possibly add the "winner" as the symbol chosen
+	this.addSymbolsToStrip = function(jqoSlider, whichReel, shouldWin){
+		var symbolsToUse = that.sameSymbolsEachSlot ? that.symbols : that.symbols[whichReel];
+		var chosen =  shouldWin ? that.winningSet[whichReel] : Math.floor(Math.random()*symbolsToUse.length);
+		for(var i = 0; i < that.howManySymbolsToAppend; i++){
+			var ctr = (i == that.endingLocation) ? chosen : Math.floor(Math.random()*symbolsToUse.length);
+			//we nest "content" inside of "symbol" so we can do vertical and horizontal centering more easily
+			var jqoContent = $("<div class='content'>"+symbolsToUse[ctr]+"</div>");
+			that.scaleJqo(jqoContent);
+			var jqoSymbol = $("<div class='symbol'></div>");
+			that.scaleJqo(jqoSymbol);
+			jqoSymbol.append(jqoContent);
+			jqoSlider.append(jqoSymbol);
+		}
+		return chosen;
+	}
+	//to spin, we add symbols to a strip, and then bounce it down
+	this.spinOne = function(jqoSlider,whichReel,shouldWin){
+		var heightBefore = parseInt(jqoSlider.css("height"), 10); 
+		var chosen = that.addSymbolsToStrip(jqoSlider,whichReel,shouldWin);
+		var marginTop = -(heightBefore + ((that.endingLocation) * that.height));
+		jqoSlider.stop(true,true).animate(
+			{"margin-top":marginTop+"px"},
+			{'duration' : that.time + Math.round(Math.random()*1000), 'easing' : "easeOutElastic"});
+		return chosen;
+	}
+
+	this.spinAll = function(shouldWin){
+		var results = [];
+		for(var i = 0; i < that.reelCount; i++){
+				results.push(that.spinOne(that.jqoSliders[i],i,shouldWin));
+			}
+		return results;
+	}
+
+	this.init();
+	return {
+		spin : function(){
+			return that.spinAll();
+		},
+		win : function(){
+			return that.spinAll(true);
+		}
+	}
+}
+
+
